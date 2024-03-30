cc54fd2b8edf33886bbc8338f184ec3220cc1457
João <joao.ferraz.balducci@gmail.com>
2024-03-30 16:22:48 -0300

    Login com Authentication funcionando

--- app/src/main/java/com/app/simon/LoginActivity.kt
+++ app/src/main/java/com/app/simon/LoginActivity.kt
@@ -1,12 +1,20 @@
 package com.app.simon
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.app.simon.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +23,33 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        binding.btnEntrar.setOnClickListener {
+
+            auth.signInWithEmailAndPassword(
+                binding.etRa.text.toString(),
+                binding.etSenha.text.toString()
+            )
+                .addOnCompleteListener(this) { task ->
+                    if(task.isSuccessful){
+                        val user = auth.currentUser
+                        Toast.makeText(
+                            baseContext, "Login realizado para usuário " + user!!.email,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            baseContext, "Email ou senha incorreta, tente novamente!",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
         }
     }
 }
